bdf6a2c6b56dc093ed167f9101f60518e3cd317a
18260631810 <18260631810@163.com>
2018-05-08 00:42:31 +0800

    购物车实例

--- src/components/Main.js
+++ src/components/Main.js
@@ -22,9 +22,9 @@ const Basic = () => (
     <div className="clear container-main">
       <div className="fl">
         <Menu
-          style={{width: 240}}
+          style={{width: 160}}
           defaultSelectedKeys={['1']}
-          defaultOpenKeys={['sub1']}
+          defaultOpenKeys={['sub2']}
           mode="inline"
         >
           <SubMenu key="sub1" title={<span><Icon type="mail"/><span>操作</span></span>}>
@@ -32,6 +32,11 @@ const Basic = () => (
             <Menu.Item key="2"><Link to="/about">购物车</Link></Menu.Item>
             <Menu.Item key="3"><Link to="/topics">购买记录</Link></Menu.Item>
             <Menu.Item key="4"><Link to="/aboutus">关于</Link></Menu.Item>
+          </SubMenu>
+
+          <SubMenu key="sub2" title={<span><Icon type="aliwangwang-o" /><span>评论</span></span>}>
+            <Menu.Item key="1">评论列表</Menu.Item>
+            <Menu.Item key="2">发表评论</Menu.Item>
           </SubMenu>
         </Menu>
       </div>
